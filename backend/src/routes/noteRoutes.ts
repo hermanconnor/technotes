ff3d4480eed04e11f5584b7f7cc7b0482aad37b0
herman <hermanconnor11@gmail.com>
2026-03-12 16:28:05 -0400

    Refactor zod note schema and add schema for updating a note

--- backend/src/routes/noteRoutes.ts
+++ backend/src/routes/noteRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getAllNotes } from '../controllers/noteController.js';
+import { createNote, getAllNotes } from '../controllers/noteController.js';
 
 const router = Router();
 
 router.get('/', getAllNotes);
+
+router.post('/', createNote);
 
 export default router;
--- backend/src/validation/noteSchema.ts
+++ backend/src/validation/noteSchema.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod';
 import { Types } from 'mongoose';
 
+const objectIdSchema = z.string().refine((val) => Types.ObjectId.isValid(val), {
+  message: 'Invalid ID format',
+});
+
 export const noteSchema = z.object({
-  user: z.string().refine((val) => Types.ObjectId.isValid(val), {
-    message: 'Invalid User ID format',
-  }),
+  user: objectIdSchema,
   title: z
     .string()
     .min(3, 'Title must be at least 3 characters')
@@ -13,3 +15,24 @@ export const noteSchema = z.object({
   text: z.string().min(1, 'Text is required').trim(),
   completed: z.boolean().optional(),
 });
+
+export const updateNoteSchema = z
+  .object({
+    id: objectIdSchema,
+    user: objectIdSchema.optional(),
+    title: z
+      .string()
+      .min(3, 'Title must be at least 3 characters')
+      .max(100, 'Title must be under 100 characters')
+      .trim()
+      .optional(),
+    text: z.string().min(1, 'Text is required').trim().optional(),
+    completed: z.boolean().optional(),
+  })
+  .refine(
+    (data) =>
+      data.title || data.text || data.user || data.completed !== undefined,
+    {
+      message: 'At least one field must be provided for update',
+    },
+  );
