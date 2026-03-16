893e5276898384b3874ac6f4f8e30bb15a69f87f
herman <hermanconnor11@gmail.com>
2026-03-15 21:47:13 -0400

    Add helmet, cors, compression, and cookieparser middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,24 @@
 import express, { type Application } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { globalErrorHandler } from './middleware/globalErrorHandler.js';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import noteRoutes from './routes/noteRoutes.js';
+import corsOptions from './config/corsOptions.js';
 
 const app: Application = express();
 
 // MIDDLEWARE
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(compression());
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan('dev'));
 
 // HEALTH CHECK
